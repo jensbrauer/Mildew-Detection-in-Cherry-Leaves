9cad66defb423ef0908dad0289e39e584d3260f0
jensbrauer <henningsson.jens@gmail.com>
2023-06-05 08:12:03 +0000

    Remove section colors for regular text

--- app_pages/project_background.py
+++ app_pages/project_background.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 def project_background_body():
-    st.write("### Project Background")
+    st.header("Project Background")
 
-    st.info(
+    st.write(
         f"**General Information**\n"
         f"* Powdery mildew is a fungal disease that affects cherry trees, and a wide range of other plants.\n"
         f"* The process to verify if a given cherry tree is infected with powdery mildew or not "
@@ -12,14 +12,14 @@ def project_background_body():
         f"requires a scalable solution to ensure that the market is not supplied "
         f"with a product of compromised quality. \n")
 
-    st.success(
+    st.info(
         f"**Business Requirements**\n"
         f"* 1 - The client is interested in having a study to differentiate "
         f"a parasitized and uninfected cell visually.\n"
         f"* 2 - The client is interested in telling whether a given cell contains a malaria parasite or not. "
         )
 
-    st.info(
+    st.write(
         f"**Project Dataset**\n"
         f"* The dataset available for the project contains 4208 images in .jpeg format, "
         f" taken from the client's crop fields. The dataset include two classes of images "
@@ -30,6 +30,6 @@ def project_background_body():
         f"[this Kaggle endpoint](https://www.kaggle.com/codeinstitute/cherry-leaves).")
 
     st.write(
-        f"* More information about the project can be found in the "
-        f"[Project README file](https://github.com/jensbrauer/Mildew-Detection-in-Cherry-Leaves/blob/main/README.md).")
+        f"__*More information about the project can be found in the *__"
+        f"[__*Project README file*__](https://github.com/jensbrauer/Mildew-Detection-in-Cherry-Leaves/blob/main/README.md).")
     
