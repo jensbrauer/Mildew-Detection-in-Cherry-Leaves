bcf85294bfd7384897ff7fb0222c7c6ca2e41233
jensbrauer <henningsson.jens@gmail.com>
2023-06-08 10:30:17 +0000

    Change info sections to blue background

--- app_pages/summary_results.py
+++ app_pages/summary_results.py
@@ -12,7 +12,7 @@ def summary_results_body():
             f"*The client is interested in conducting a study to visually differentiate a healthy cherry leaf from one with powdery mildew.*\n")
 
     if st.checkbox("-- HYPOTHESIS 1 --  Cherry leaf with powdery mildew have visual signs that diffirentiate them from healthy cherry leaves."):
-        st.success(
+        st.info(
             f"**General Information**\n\n"
             f" *A visual study was conducted including plotting a set of samples"
             f" for each class as well as plotting the avarage and variability for"
@@ -26,12 +26,12 @@ def summary_results_body():
             f" be found when studying image avarage and image variability"
             )
 
-#Business Rquirement 2
+# Business Rquirement 2
     st.write(f"### Business rquirement 2 - Satisfied\n"
             f"*The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew.*")
 
     if st.checkbox("-- HYPOTHESIS 2 --  A convolutional neural network (CNN) for image classification, can predict if a cherry leaf is healthy or have powdery mildew."):
-        st.success(
+        st.info(
             f"**General Information**\n\n"
             f" *A CNN was buildt, trained and evaluated using the dataset provided by the client."
             f" Technical information about model architecture, data augmentation, performance etc."
@@ -42,7 +42,7 @@ def summary_results_body():
             f" when it was tested with data that is excluded in model training")
 
     if st.checkbox("-- HYPOTHESIS 3 --  The probability that the model assigns to a label is represantative of the probability that the prediction is correct."):
-        st.success(
+        st.info(
             f"**General Information**\n\n"
             f" *The model that was buildt, outputs two decimal numbers for each image."
             f" that sum up to 1. These numbers represent the probability that the image"
