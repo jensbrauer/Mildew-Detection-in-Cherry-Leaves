11556784bbac94f49f4dcff4f62c5e692833ccdb
jensbrauer <henningsson.jens@gmail.com>
2023-06-13 17:47:17 +0000

    add file uploader and image list view

--- app_pages/mildew_detector.py
+++ app_pages/mildew_detector.py
@@ -1,4 +1,30 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
+import pandas as pd
+
+from src.machine_learning.machine_learning import (
+                                                    size_img_for_model
+                                                    )
+
 
 def mildew_detector_body():
     st.header("Mildew Detector")
+    
+    img_dump = st.file_uploader('Upload one or more cherry leaves for mildew diagnostics.',
+                                        type='png',accept_multiple_files=True)
+
+    if img_dump is not None:
+        df_report = pd.DataFrame([])
+        st.header('View results as:')
+        format = st.radio('Select what class you want to view image samples from:', ('Images', 'Powdery Mildew'))
+        st.write('---')
+        if format == 'Images':
+            for img in img_dump:
+                img_pil = (Image.open(img))
+                
+                img_array = np.array(img_pil)
+                st.image(size_img_for_model(img=img_pil)[1], caption=f"Image name: {img.name}")
+                st.write('---')
+        elif format == 'Powdery Mildew':
+            st.write('hej')
---
+++ src/machine_learning/machine_learning.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from tensorflow.keras.models import load_model
+from PIL import Image
+
+def size_img_for_model(img):
+    """
+    Reshape image to average image size
+    """
+    img_model_shape = (75, 75, 3)
+    img_display_shape = (200, 200, 3)
+    #image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
+    img_display_resized = img.resize((img_display_shape[1], img_display_shape[0]), Image.ANTIALIAS)
+    display_input_img = np.expand_dims(img_display_resized, axis=0)/255
+    
+    img_model_resized = img.resize((img_model_shape[1], img_model_shape[0]), Image.ANTIALIAS)
+    model_input_img = np.expand_dims(img_model_resized, axis=0)/255
+
+    return model_input_img, display_input_img
