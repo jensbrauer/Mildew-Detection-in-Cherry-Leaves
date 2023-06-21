9e879d3b9a9b767ecbe51b47dee020afa02714c9
jensbrauer <henningsson.jens@gmail.com>
2023-06-21 05:00:20 +0000

    Rewrite business requirements

--- app_pages/project_background.py
+++ app_pages/project_background.py
@@ -5,18 +5,20 @@ def project_background_body(model_version):
 
     st.write(
         f"**General Information**\n"
+        f"* The project owner, a company managing thousands of cherry trees located in multiple farms across the country, "
+        f"requires a scalable solution to ensure a healthy crop and that the market is not supplied "
+        f"with a product of compromised quality. \n"
         f"* Powdery mildew is a fungal disease that affects cherry trees, and a wide range of other plants.\n"
         f"* The process to verify if a given cherry tree is infected with powdery mildew or not "
         f"involves an employee, taking a few samples of leaves from the tree and visually examining them.\n"
-        f"* For a company, managing thousands of cherry trees located in multiple farms across the country "
-        f"requires a scalable solution to ensure that the market is not supplied "
-        f"with a product of compromised quality. \n")
+        f" * Therefore, this project is aimed at investigating the feasibility of employing Machine learning,"
+        f" for the task of detecting powdery mildew in cherry leaves. ")
 
     st.info(
         f"**Business Requirements**\n"
-        f"* 1 - The client is interested in having a study to differentiate "
-        f"a parasitized and uninfected cell visually.\n"
-        f"* 2 - The client is interested in telling whether a given cell contains a malaria parasite or not. "
+        f"* 1 - The client is interested in conducting a study to visually "
+        f"differentiate a healthy cherry leaf from one with powdery mildew.\n"
+        f"* 2 - The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew. "
         )
 
     st.write(
@@ -26,7 +28,7 @@ def project_background_body(model_version):
         f"with 2104 images each. The classes are labeled 'Powdery Mildew' for images of leaves "
         f"that are infected with the fungal desease and 'Healthy' for images of leaves "
         f"that are uninfected.\n"
-        f"* The dataset can be found at "
+        f'* The dataset is further explored in "Model Specifications" and the dataset itself can be found at '
         f"[this Kaggle endpoint](https://www.kaggle.com/codeinstitute/cherry-leaves).")
 
     st.write(
